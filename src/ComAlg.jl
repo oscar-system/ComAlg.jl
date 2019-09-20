756b26e9ffb80a755b735aedb3be6c1f0934e527
wbhart <goodwillhart@googlemail.com>
2019-09-20 15:21:31 +0200

    Update ComAlg.jl

--- src/ComAlg.jl
+++ src/ComAlg.jl
@@ -1,4 +1,48 @@
+module ComAlg
+
+
+# To make all exported Nemo/Heck functions visible to someone using "using Hecke"
+# we have to export everything again
+
+exclude = [:QQ, :ZZ, :Nemo, :AbstractAlgebra, :Hecke, :Singular]
+
+import AbstractAlgebra
+import Nemo
+import Hecke
+
+for i in names(Hecke)  # includes Nemo (and AbstractAlgebra??)
+  i in exclude && continue 
+  eval(Expr(:import, :Hecke, i))
+  eval(Expr(:export, i))
+end
+
+import Singular
+
+exclude = [:QQ, :ZZ, :Singular, :Ring, :Set, :Module, :Nemo, :AbstractAlgebra, :Hecke]
+for i in names(Singular)  # includes Nemo (and AbstractAlgebra??)
+  i in exclude && continue 
+  eval(Expr(:import, :Singular, i))
+  eval(Expr(:export, i))
+end
+
+
+println("Welcome to ComAlg")
+println(" ... version 0.0.2")
+println(" ... there is absolutely no warranty...")
+println("(c) 2018 by Claus Fieker, Andreas Steenpass")
+
 include("ComAlg/Types.jl")
 #include("ComAlg/Ideal.jl")
 include("ComAlg/ModFld.jl")
 include("ComAlg/ModSubquo.jl")
+
+Make all Map-s callable...
+for T in subtypes(Map)
+  if length(methods(T)) == 0 # to avoid re-doing the Hecke types
+    (M::T)(a) = image(M, a)
+  end  
+end
+
+end # module
+
+
